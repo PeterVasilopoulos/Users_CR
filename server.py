fe97bd52d22b749f4e1249e9f4c1fbe078a1fc31
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-12 19:38:37 -0400

    Finished base assignment

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 from mysqlconnection import connectToMySQL
 
@@ -9,14 +9,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    results = connectToMySQL("users").query_db("SELECT * FROM users")
-    print(results)
+    user_list = User.get_all()
 
-    return render_template('index.html')
+    return render_template('index.html', user_list = user_list)
 
 @app.route('/create')
 def create():
     return render_template('create.html')
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    User.create(request.form)
+    return redirect('/')
 
 
 if __name__ == "__main__":
--- user_model.py
+++ user_model.py
@@ -1,6 +1,35 @@
+from mysqlconnection import connectToMySQL
+
+DATABASE = "users"
+
 class User:
     def __init__(self, data):
         self.first_name = data['first_name']
         self.last_name = data['last_name']
         self.email = data['email']
-        
+
+    @classmethod
+    def get_all(cels):
+        query = """
+            SELECT * FROM users;
+        """
+
+        results = connectToMySQL(DATABASE).query_db(query)
+
+        users = []
+
+        if results:
+            for dict in results:
+                new_user = User(dict)
+                users.append(new_user)
+
+        return users
+
+    @classmethod
+    def create(cls, data):
+        query = """
+            INSERT INTO users(first_name, last_name, email) 
+            VALUES(%(first_name)s, %(last_name)s, %(email)s)
+        """
+
+        return connectToMySQL(DATABASE).query_db(query, data)
