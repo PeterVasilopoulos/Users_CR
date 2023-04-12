b53487cbde83ab051ec41753c9c8f50f312df318
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-12 15:36:23 -0400

    Added a few files

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
+from mysqlconnection import connectToMySQL
+
 app = Flask(___name__)
 
 @app.route('/')
 def index():
+
+    results = connectToMySQL("users").query_db("SELECT * FROM users")
+    print(results)
+
     return render_template('index.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
